caec16482a39cec428d1a6f37869659e96ecea8c
Kir Mesh <567@city-call.ru>
2015-09-04 08:20:57 +0500

    add ignore folders, files, et.

---
+++ docs/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="cp1251">
+	<title>Doc-file</title>
+</head>
+<body>
+	<h1>GIT touch 5</h1>
+	<p>Start line here.</p>
+	<div>
+		Здесь мог быть какой-то текст.
+	</div>
+	<p>End line here.</p>
+</body>
+</html>
